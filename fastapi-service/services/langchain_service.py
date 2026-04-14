cefff8cd29029e373f160f8153b2149bcf0d6214
Devansh Pandey <devanshpandey170405@gmail.com>
2026-04-14 15:53:10 +0530

    changed langchain service for gemini

--- fastapi-service/services/langchain_service.py
+++ fastapi-service/services/langchain_service.py
@@ -1,13 +1,24 @@
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+from langchain_core.output_parsers import StrOutputParser
+
+from config import config
 
 
 class LangChainService:
     def __init__(self):
-        self.llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+        self.llm = ChatGoogleGenerativeAI(
+            model="gemini-2.5-flash",
+            temperature=0.1,
+            max_output_tokens=512,  # max tokens in output
+            google_api_key=config.GEMINI_API_KEY,
+        )
 
         self.prompt = PromptTemplate.from_template(
-            """Answer ONLY from the given context.
+            """Use ONLY the provided context to answer the question.
+If the answer is not explicitly in the context, say "I don't know".
+Do not infer or make up information.
 
 Context:
 {context}
@@ -18,12 +29,16 @@ Question:
 Answer:"""
         )
 
+        self.chain = self.prompt | self.llm | StrOutputParser()
+
     def generate(self, query: str, context_chunks: list[str]):
+        try:
+            if not context_chunks:
+                return "I don't know."
 
-        context = "\n\n".join(context_chunks)
+            context = "\n\n--\n\n".join(context_chunks)
 
-        formatted_prompt = self.prompt.format(context=context, question=query)
+            return self.chain.invoke({"context": context, "question": query})
 
-        response = self.llm.invoke(formatted_prompt)
-
-        return response.content
+        except Exception as e:
+            return f"Error generating response: {str(e)}"
